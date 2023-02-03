747cd10714d4847e801bb6143adbeb5068d0ca62
mayurValsangkar <49154257+mayurValsangkar@users.noreply.github.com>
2023-02-03 23:59:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+var navMenuAnchorTags = document.querySelectorAll('.nav-menu a');
+
+for(var i = 0; i < navMenuAnchorTags.length; i++){
+    navMenuAnchorTags[i].addEventListener('click', function(event){
+        event.preventDefault();
+        var targetSectionID = this.textContent.trim().toLowerCase();
+        var targetSection = document.getElementById(targetSectionID);
+        var interval = setInterval(function(){
+            var targetSectionCordinate = targetSection.getBoundingClientRect();
+            if(targetSectionCordinate.top <= 0){
+                clearInterval(interval);
+                return;
+            }
+            window.scrollBy(0, 55);
+        }, 25);
+    });
+}
